818cc2ba8c9fc4398d590498e67ea29f5465a496
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-03-29 02:22:41 +0530

    v1.1

--- arkham/settings.py
+++ arkham/settings.py
@@ -81,12 +81,25 @@ WSGI_APPLICATION = 'arkham.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'arkham1',
+        'USER': 'postgres',
+        'PASSWORD': 'Chandra',
+        'HOST': 'localhost'
+
     }
 }
+
+
 
 
 # Password validation
