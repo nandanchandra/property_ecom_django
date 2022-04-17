2e740b0deebf58c5f8bc0ceddf957b3975872eb1
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-04-18 03:44:38 +0530

    r1.0

--- arkham/settings.py
+++ arkham/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,23 +82,23 @@ WSGI_APPLICATION = 'arkham.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'arkham1',
-        'USER': 'postgres',
-        'PASSWORD': 'Chandra',
-        'HOST': 'localhost'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'arkham1',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Chandra',
+#         'HOST': 'localhost'
+
+#     }
+# }
 
 
 
@@ -158,3 +159,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+django_heroku.settings(locals())
